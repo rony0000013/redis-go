025d92fc9d165e79f9eb08b04f07d90966dc076b
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2025-06-12 00:12:30 +0530

    added multiple pong response

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -12,13 +13,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
-
-	conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading from connection:", err.Error())
+			break
+		}
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
