746be8f97e42fe75391466cfb51f5bc88e81f3fc
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2025-07-18 12:57:19 +0530

    removed sending extra data

--- app/replication/replication.go
+++ app/replication/replication.go
@@ -2,11 +2,9 @@ package replication
 
 import (
 	"net"
-
-	"github.com/codecrafters-io/redis-starter-go/app/resp"
 )
 
 func SendHandshake(conn net.Conn) {
 	conn.Write([]byte("*1\r\n$4\r\nPING\r\n"))
-	conn.Write(resp.ToSimpleString("*2\r\n$11\r\nREPLCONF\r\n$11\r\nSETNAME\r\n"))
+	// conn.Write(resp.ToSimpleString("*2\r\n$11\r\nREPLCONF\r\n$11\r\nSETNAME\r\n"))
 }
